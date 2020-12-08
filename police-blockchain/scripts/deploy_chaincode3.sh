2303629ff09a02e19b77d5b8b2f4579e83b6f0fd
ralph <ralpharvin.dc@gmail.com>
2020-12-08 01:36:18 -0500

    update chaincode

--- police-blockchain/scripts/deploy_chaincode3.sh
+++ police-blockchain/scripts/deploy_chaincode3.sh
@@ -231,6 +231,17 @@ chaincodeInvokeInit() {
       --peerAddresses localhost:7051 --tlsRootCertFiles $PEER0_ORG1_CA \
       --peerAddresses localhost:9051 --tlsRootCertFiles $PEER0_ORG2_CA \
       -c '{"Args":["UpdateDataHashForSale", "abc"]}'
+
+  sleep 2
+
+  setGlobalsForPeer0Org1
+  peer chaincode invoke -o localhost:7050 \
+      --ordererTLSHostnameOverride orderer.example.com \
+      --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA \
+      -C $CHANNEL_NAME -n ${CC_NAME} \
+      --peerAddresses localhost:7051 --tlsRootCertFiles $PEER0_ORG1_CA \
+      --peerAddresses localhost:9051 --tlsRootCertFiles $PEER0_ORG2_CA \
+      -c '{"Args":["QueryAccount", "ralph"]}'
   echo "===================== Ledger Initiated ===================== "
 
 }
